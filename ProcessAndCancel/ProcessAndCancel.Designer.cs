5c559a89e3278d8b9aff64e6b8d8a75fedfde6cf
Qinghang Wu <Qinghang Wu>
2016-09-26 16:57:19 +1000

    Process and Cancel

---
+++ ProcessAndCancel/ProcessAndCancel.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ProcessAndCancel
+{
+    partial class ProcessAndCancel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 163);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(317, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(254, 50);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(254, 101);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(25, 55);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(95, 12);
+            this.lblInstruction.TabIndex = 3;
+            this.lblInstruction.Text = "ToBeInitialized";
+            // 
+            // ProcessAndCancel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 198);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ProcessAndCancel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Process And Cancel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblInstruction;
+    }
+}
+
---
+++ ProcessAndCancel/ProcessAndCancel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProcessAndCancel
+{
+    public partial class ProcessAndCancel : Form
+    {
+        CancellationTokenSource _cancellationTokenSource;
+        CancellationToken _cancellationToken;
+        public ProcessAndCancel()
+        {
+            InitializeComponent();
+            lblInstruction.Text = "Instruction:\n\nPress Process Button To Start\n\nPress Cancel Button To Stop";
+            btnCancel.Enabled = false;
+        }
+
+        private async void btnProcess_Click(object sender, EventArgs e)
+        {
+            btnProcess.Enabled = false;
+            btnCancel.Enabled = true;
+            _cancellationTokenSource = new CancellationTokenSource();
+            _cancellationToken = _cancellationTokenSource.Token;
+
+            int completedPercent = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if(_cancellationToken.IsCancellationRequested == true)
+                    break;
+                try
+                {
+                    await Task.Delay(200, _cancellationToken);
+                    completedPercent = i + 1;
+                }
+                catch (TaskCanceledException)
+                {
+                    completedPercent = i;
+                }
+                progressBar.Value = completedPercent;
+            }
+
+            string message = _cancellationToken.IsCancellationRequested
+                ? string.Format("Process was cancelled at {0}%.", completedPercent)
+                : string.Format("Process completed normally.");
+            MessageBox.Show(message, "Completion Status");
+            progressBar.Value = 0;
+            btnCancel.Enabled = false;
+            btnProcess.Enabled = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+            btnCancel.Enabled = false;
+            btnProcess.Enabled = true;
+
+        }
+    }
+}
